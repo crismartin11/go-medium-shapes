dafd58b0b1ff7a287a45f36a0badec8b3a94729e
crismartin11 <cristian.utnsistemas@gmail.com>
2023-09-27 22:53:34 -0300

    change lambda name

--- build.bat
+++ build.bat
@@ -2,10 +2,12 @@
 cls
 echo :: Building project.
 
+set lambdaName="lambdaShapesCris"
+
 set GOOS=linux
 set GOARCH=amd64
 set CGO_ENABLED=0
-go build -o build/main cmd/main.go
-%USERPROFILE%\Go\bin\build-lambda-zip.exe -o build/main.zip build/main
+go build -o build/%lambdaName% cmd/main.go
+%USERPROFILE%\Go\bin\build-lambda-zip.exe -o build/%lambdaName%.zip build/%lambdaName%
 
 echo :: Build finished.
