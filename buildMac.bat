e398b93df3495456bad7c03a5768bae545b48911
crismartin11 <cristian.utnsistemas@gmail.com>
2023-10-06 22:53:57 -0300

    Update bucket name

--- buildMac.bat
+++ buildMac.bat
@@ -5,6 +5,7 @@ echo Building project.
 lambdaName="goMediumShapes"
 directory="package"
 
+mkdir ./build/package
 cd ./cmd
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o $lambdaName main.go
 zip -rm ../build/$directory/$lambdaName.zip $lambdaName
